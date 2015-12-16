b262fbb97930ef880254307a75f298aa86acd12a
Matt Senior <matt@mattsenior.com>
2015-12-16 23:43:43 +0000

    Additional password constraints

--- src/adventofcode_devcards/day-11.cljs
+++ src/adventofcode_devcards/day-11.cljs
@@ -10,7 +10,32 @@
 (defn correct-length? [x]
   (= 8 (count x)))
 
-(def valid? (comp correct-length?))
+(defn has-straight? [[a & z]]
+  (let [[b c & y] z]
+    (if (and (= 1 (- a b)) (= 1 (- b c)))
+      true
+      (if (nil? y) false (has-straight? z)))))
+
+(def i 8)
+(def o 14)
+(def l 11)
+(def illegal-characters #{i o l})
+
+(defn valid-characters? [x]
+  (not-any? illegal-characters x))
+
+(defn has-pairs? [x]
+  (->> x
+       (partition-by identity)
+       (filter #(> (count %) 1))
+       (map first)
+       set
+       count
+       (<= 2)))
+
+(def valid? (every-pred correct-length?
+                        has-straight?
+                        valid-characters?))
 
 ;; Password sequences
 
@@ -47,8 +72,29 @@
     (is (not (correct-length? (str->pw "xxx"))))
     (is (not (correct-length? (str->pw "xxxxxxxxx"))))
     (is (correct-length? (str->pw "xxxxxxxx"))))
+  (testing "has-straight?"
+    (is (= true (has-straight? (str->pw "abc"))))
+    (is (= true (has-straight? (str->pw "bcd"))))
+    (is (= true (has-straight? (str->pw "xyz"))))
+    (is (= true (has-straight? (str->pw "zabc"))))
+    (is (= true (has-straight? (str->pw "abcz"))))
+    (is (= true (has-straight? (str->pw "zabcz"))))
+    (is (= false (has-straight? (str->pw "abd"))))
+    (is (= false (has-straight? (str->pw "aaa"))))
+    (is (= false (has-straight? (str->pw "yyz"))))
+    )
+  (testing "valid-characters?"
+    (is (= true (valid-characters? (str->pw "abcdefghjkmnpqrstuvwxyz"))))
+    (is (= false (valid-characters? (str->pw "i"))))
+    (is (= false (valid-characters? (str->pw "o"))))
+    (is (= false (valid-characters? (str->pw "l"))))
+    (is (= false (valid-characters? (str->pw "abcdefghijkl")))))
+  (testing "has-pairs?"
+    (is (= true (has-pairs? (str->pw "aacc"))))
+    (is (= true (has-pairs? (str->pw "aabcc"))))
+    (is (= false (has-pairs? (str->pw "aabaa")))))
   (testing "valid?"
-    (is (valid? (str->pw "xxxxxxxx"))))
+    (is (valid? (str->pw "xxabcxxx"))))
   (testing "Part 1"
     (is (= "y" (->> "x" str->pw passwords second pw->str)))
     (is (= "xy" (->> "xx" str->pw passwords second pw->str)))
